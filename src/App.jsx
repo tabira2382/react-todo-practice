527825180b3cc8c88457743ea3e33d9d5bf7d19a
tabira2382 <tabira2382@gmail.com>
2025-03-17 09:54:08 +0900

    add:イベント、スタイルテスト

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 
 export const App = () => {
+  const onClickButton = () => alert("ボタンを押しました");
+  const conterntStyle = {
+    color: "blue",
+    fontSize: "18px"
+  }
   return (
     <>
-      <h1>こんにちは!</h1>
-      <p>お元気ですか？</p>
+      <h1 style={{ color: "red"}}>こんにちは!</h1>
+      <p style={conterntStyle}>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   )
 };
