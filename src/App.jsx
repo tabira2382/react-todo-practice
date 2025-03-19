bf97b180b1522897d26c063f3e6bf5e14e229584
tabira2382 <tabira2382@gmail.com>
2025-03-19 15:32:49 +0900

    useEffect実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ColorfulMessage } from "./components/ColorfulMessage";
 
 export const App = () => {
+  console.log("--APP--")
   const [num, setNum] = useState(0);
+  const [isShowFace, setIsShowFace] = useState(false);
   const onClickCountUp = () => {
     setNum((prev) => prev + 1);
-    setNum(num + 1);
   };
+  const onClickToggle = () => {
+    setIsShowFace(!isShowFace);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        isShowFace || setIsShowFace(true);
+      } else {
+        isShowFace && setIsShowFace(false);
+      }
+    }
+  },[num]);
+
   return (
     <>
       <h1 style={{ color: "red"}}>こんにちは!</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="green">元気です！</ColorfulMessage>
       <button onClick={onClickCountUp}>カウントアップ</button>
-      <p>{num}</p>
+      <p>{num}</ p>
+      <button onClick={onClickToggle}>on/off</button>
+      {isShowFace && <p>^_^</p>}
     </>
   )
 };
